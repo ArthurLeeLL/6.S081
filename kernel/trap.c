eb0b0c72a8240b85679db1aea5b87cd18c6c130b
ArthurLeeLL <xingyuli1996@gmail.com>
2024-07-22 22:03:22 +0800

    Lab: xv6 lazy page allocation finished

--- kernel/trap.c
+++ kernel/trap.c
@@ -65,6 +65,30 @@ usertrap(void)
     intr_on();
 
     syscall();
+  } else if(r_scause() == 13 || r_scause() == 15){
+      char *pa;
+      uint64 va = r_stval();
+      if(va >= p->sz) {
+          printf("usertrap(): invalid va=%p higher than p->sz=%p\n", va, p->sz);
+          p->killed = 1;
+          goto end;
+      }
+      if(va < PGROUNDUP(myproc()->trapframe->sp)) {
+          printf("usertrap(): invalid va%p below the user stack sp=%p\n", va, p->trapframe->sp);
+          p->killed = 1;
+          goto end;
+      }
+      if((pa=kalloc()) == 0) {
+          printf("usertrap(): kalloc() failed\n");
+          p->killed = 1;
+          goto end;
+      }
+      memset(pa, 0, PGSIZE);
+      if(mappages(p->pagetable, PGROUNDDOWN(va), PGSIZE, (uint64)pa, PTE_W|PTE_R|PTE_U) != 0) {
+          kfree(pa);
+          p->killed = 1;
+          goto end;
+      }
   } else if((which_dev = devintr()) != 0){
     // ok
   } else {
@@ -73,6 +97,7 @@ usertrap(void)
     p->killed = 1;
   }
 
+end:
   if(p->killed)
     exit(-1);
 
--- kernel/vm.c
+++ kernel/vm.c
@@ -5,6 +5,8 @@
 #include "riscv.h"
 #include "defs.h"
 #include "fs.h"
+#include "spinlock.h"
+#include "proc.h"
 
 /*
  * the kernel's page table.
@@ -88,27 +90,61 @@ walk(pagetable_t pagetable, uint64 va, int alloc)
   return &pagetable[PX(0, va)];
 }
 
+//// Look up a virtual address, return the physical address,
+//// or 0 if not mapped.
+//// Can only be used to look up user pages.
+//uint64
+//walkaddr(pagetable_t pagetable, uint64 va)
+//{
+//  pte_t *pte;
+//  uint64 pa;
+//
+//  if(va >= MAXVA)
+//    return 0;
+//
+//  pte = walk(pagetable, va, 0);
+//  if(pte == 0)
+//    return 0;
+//  if((*pte & PTE_V) == 0)
+//    return 0;
+//  if((*pte & PTE_U) == 0)
+//    return 0;
+//  pa = PTE2PA(*pte);
+//  return pa;
+//}
+
 // Look up a virtual address, return the physical address,
 // or 0 if not mapped.
 // Can only be used to look up user pages.
 uint64
 walkaddr(pagetable_t pagetable, uint64 va)
 {
-  pte_t *pte;
-  uint64 pa;
+    pte_t *pte;
+    uint64 pa;
 
-  if(va >= MAXVA)
-    return 0;
+    if(va >= MAXVA)
+        return 0;
 
-  pte = walk(pagetable, va, 0);
-  if(pte == 0)
-    return 0;
-  if((*pte & PTE_V) == 0)
-    return 0;
-  if((*pte & PTE_U) == 0)
-    return 0;
-  pa = PTE2PA(*pte);
-  return pa;
+    pte = walk(pagetable, va, 0);
+    if(pte == 0 || (*pte & PTE_V) == 0) {
+        if(va >= PGROUNDUP(myproc()->trapframe->sp) && va < myproc()->sz) {
+            char  *pa;
+            if((pa=kalloc()) == 0) {
+                return 0;
+            }
+            memset(pa, 0, PGSIZE);
+            if(mappages(myproc()->pagetable, PGROUNDDOWN(va), PGSIZE, (uint64)pa, PTE_W|PTE_R|PTE_U) != 0) {
+                kfree(pa);
+                return 0;
+            }
+        }else {
+            return 0;
+        }
+    }
+    if((*pte & PTE_U) == 0)
+        return 0;
+    pa = PTE2PA(*pte);
+    return pa;
 }
 
 // add a mapping to the kernel page table.
@@ -181,9 +217,11 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
 
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
-      panic("uvmunmap: walk");
+//      panic("uvmunmap: walk");
+          continue;
     if((*pte & PTE_V) == 0)
-      panic("uvmunmap: not mapped");
+//      panic("uvmunmap: not mapped");
+      continue;
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
@@ -315,9 +353,11 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
-      panic("uvmcopy: pte should exist");
+//      panic("uvmcopy: pte should exist");
+          continue;
     if((*pte & PTE_V) == 0)
-      panic("uvmcopy: page not present");
+//      panic("uvmcopy: page not present");
+          continue;
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
     if((mem = kalloc()) == 0)
